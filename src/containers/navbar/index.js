9d2b63227f6ed3d04d5d9dd2a19693eca2648aa1
darlok77 <g.dalleau77@outlook.fr>
2020-02-20 14:32:26 +0100

    refacto(navBar WIP)

--- src/containers/navbar/index.js
+++ src/containers/navbar/index.js
@@ -1,33 +1,82 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import Paper from '@material-ui/core/Paper'
-import MenuItem from '@material-ui/core/MenuItem'
-import MenuList from '@material-ui/core/MenuList'
+// import { Link } from 'react-router-dom'
+import {
+  ListItem,
+  List,
+  ListItemIcon,
+  Hidden,
+  Drawer
+} from '@material-ui/core'
 import { withStyles } from '@material-ui/core/styles'
 
 const styles = {
   root: {
-    display: 'flex'
+    display: 'flex',
+    backgroundColor: '#2F3136'
+  },
+  test: {
+    backgroundColor: '#2F3136'
   }
 }
 
 class Home extends Component {
   render() {
     const { classes } = this.props
-    return (
-      <div className={classes.root}>
-        <Paper>
-          <MenuList>
-            <MenuItem>
-              <Link to="/">Home</Link>
-            </MenuItem>
-            <MenuItem>
-              <Link to="/games">game</Link>
-            </MenuItem>
-            <MenuItem>Logout</MenuItem>
-          </MenuList>
-        </Paper>
+
+    const drawer = (
+      <div>
+        <div className={classes.toolbar} />
+        <List>
+          <ListItem button>
+            <ListItemIcon>
+              <img src="/asset/image/home.svg" alt="Home" />
+            </ListItemIcon>
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon>
+              <img src="/asset/image/home.svg" alt="Home" />
+            </ListItemIcon>
+          </ListItem>
+          <ListItem button>
+            <ListItemIcon>
+              <img src="/asset/image/home.svg" alt="Home" />
+            </ListItemIcon>
+          </ListItem>
+        </List>
       </div>
+    )
+
+    return (
+      <nav className={classes.drawer} aria-label="mailbox folders">
+        <Hidden smUp implementation="css">
+          <Drawer
+            container={container}
+            variant="temporary"
+            anchor={theme.direction === 'rtl' ? 'right' : 'left'}
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+        <Hidden xsDown implementation="css">
+          <Drawer
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            variant="permanent"
+            open
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+      </nav>
     )
   }
 }
