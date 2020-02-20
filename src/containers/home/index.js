a7d9d35ca150c6018e5d7a6557f16fd58104a8ef
darlok77 <g.dalleau77@outlook.fr>
2020-02-20 10:57:22 +0100

    feat(add card)

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -1,11 +1,63 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Typography
+} from '@material-ui/core'
+import { withStyles } from '@material-ui/core/styles'
+
+import { getUser } from './actions'
+
+const styles = {
+  root: {
+    maxWidth: 345
+  },
+  media: {
+    height: 50
+  }
+}
 
 class Home extends Component {
+  constructor(props) {
+    super(props)
+    getUser(1)
+  }
+
   render() {
+    const { user, classes } = this.props
+    console.log(user)
     return (
-      <div> home </div>
+      <div>
+      <Card className={classes.root}>
+        <CardActionArea>
+          <CardMedia
+            className={classes.media}
+            image="/static/images/cards/contemplative-reptile.jpg"
+            title="Contemplative Reptile"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">Lizard</Typography>
+            <Typography variant="body2" color="textSecondary" component="p">test</Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
+      </div>
     )
   }
 }
 
-export default Home
+/**
+  * mapStateToProps.
+  * @param {Object} state.
+  * return {Object}.
+*/
+const mapStateToProps = state => (
+  {
+    user: state.home.user
+  }
+)
+
+export default withStyles(styles)(connect(mapStateToProps)(Home))
--- src/containers/home/reducer/index.js
+++ src/containers/home/reducer/index.js
@@ -3,55 +3,16 @@ import { fromJS } from 'immutable'
 import initialState from './initial-state'
 import actionsType from '../actions/actions-type'
 
-const updatePlayer = (state, action) => (
+const getUser = (state, action) => (
   fromJS(state)
-    .setIn(['player'], action.player)
-    .toJS()
-)
-
-const updateWorld = (state, action) => (
-  fromJS(state)
-    .setIn(['world'], action.world)
-    .toJS()
-)
-
-const updateNumber = (state, action) => (
-  fromJS(state)
-    .setIn(['number'], action.number)
-    .toJS()
-)
-
-const updateTurn = (state, action) => (
-  fromJS(state)
-    .setIn(['turn'], action.turn)
-    .toJS()
-)
-
-const updateBase = (state, action) => (
-  fromJS(state)
-    .setIn(['bases'], action.bases)
-    .toJS()
-)
-const updateCurrentCase = (state, action) => (
-  fromJS(state)
-    .setIn(['currentCase'], action.currentCase)
+    .setIn(['user'], action.user)
     .toJS()
 )
 
 const home = (state = initialState, action) => {
   switch (action.type) {
-    case actionsType.NEXT_PLAYER:
-      return updatePlayer(state, action)
-    case actionsType.UPDATE_WORLD:
-      return updateWorld(state, action)
-    case actionsType.NEW_NUMBER:
-      return updateNumber(state, action)
-    case actionsType.NEW_TURN:
-      return updateTurn(state, action)
-    case actionsType.UPDATE_BASE:
-      return updateBase(state, action)
-    case actionsType.UPDATE_CURRENT_CASE:
-      return updateCurrentCase(state, action)
+    case actionsType.GET_USER:
+      return getUser(state, action)
     default:
       return state
   }
